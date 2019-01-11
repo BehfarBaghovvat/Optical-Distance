f1c54a2416c83a99f7a7ecf5140c943f196144b0
Behfar Baghovvat <46481959+BehfarBaghovvat@users.noreply.github.com>
2019-01-11 16:11:55 +0330

    Build the first part of the distance meter

--- Optical Distance/Program.cs
+++ Optical Distance/Program.cs
@@ -1,15 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Optical_Distance
 {
     class Program
     {
+
+        static long LightDistance(int time)
+        {
+            long Distance=0;
+
+            Distance = time * 300000;
+
+            System.Console.WriteLine();
+
+            System.Console.WriteLine("The distance to which light lasted for {0} seconds: {1} Km",time, Distance);
+
+            return Distance;
+
+            
+        }
+
+
+
         static void Main(string[] args)
         {
+            int Time;
+
+            string SelectTime;
+
+            System.Console.WriteLine("        ========Determine the distance traveled by light========");
+
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+
+            System.Console.WriteLine("Please specify the time unit.");
+            System.Console.WriteLine();
+            System.Console.Write
+                ("Secod[1], " + "Minute[2], " + "Hour[3], " + 
+                "Day[4], " + "Week[5], " + "Month[6], " + 
+                "Year[7], " + "Centuries[8]: ");
+            SelectTime = System.Console.ReadLine();
+
+            switch (SelectTime)
+            {
+                case "1":
+
+                    System.Console.WriteLine();
+                    System.Console.Write("Please enter the SECONDS: ");
+                    Time = int.Parse(System.Console.ReadLine());
+
+                    LightDistance(Time);
+
+
+                    break;
+                
+                
+                
+                    
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+            System.Console.WriteLine();
+            System.Console.ReadLine();
         }
     }
 }
